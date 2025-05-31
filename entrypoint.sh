82c78c572efa29ef25e08bd0b26ec5c6fb2f523c
awaaladin <awajefferson598@gmail.com>
2025-05-31 08:30:56 +0100

    Add startup script for Render

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run database migrations
+python manage.py migrate --noinput
+
+# Collect static files (so your CSS/images work)
+python manage.py collectstatic --noinput
+
+# Start the Gunicorn web server
+gunicorn banking.wsgi:application --bind 0.0.0.0:$PORT
